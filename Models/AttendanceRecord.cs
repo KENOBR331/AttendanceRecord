feea30be2463316555343a10086b894e5f6fa209
KENOBR331 <0bakeno1o8@gmail.com>
2025-05-11 08:11:06 +0900

    データ処理、モデル周りの再UP

---
+++ Models/AttendanceRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace AttendanceRecord.Models
+{
+    public class AttendanceRecord
+    {
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public DateTime? StartRestTime { get; set; }
+        public DateTime? EndRestTime { get; set; }
+        public bool IsBusinessTrip { get; set; }
+        public bool IsTripAll { get; set; }
+    }
+}
