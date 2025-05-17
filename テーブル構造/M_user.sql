ed9c24ffa52d10c69383bfb0e924dd5b3c5d8cde
KENOBR331 <0bakeno1o8@gmail.com>
2025-05-18 08:43:23 +0900

    テーブル定義だけでは足りないと思うので、 テーブル構造フォルダにSQLをUP

---
+++ テーブル構造/M_user.sql
@@ -0,0 +1,15 @@
+--*DataTitle 'マスタ_勤怠管理ユーザ'
+--*CaptionFromComment
+SELECT
+    [userid]                                    -- ユーザID
+    , [name]                                    -- ユーザ名
+    , [gender]                                  -- 0
+    , [ipaddress]                               -- ADがないため、IPアドレスで代用
+    , [affiliation]                             -- 所属(部署等を数値で判別する)
+    , [del_flg]                                 -- 1のときは削除とする
+FROM
+    [dbo].[M_user] 
+WHERE
+    [userid] = :userid                          -- ユーザID
+ORDER BY
+    [userid]
