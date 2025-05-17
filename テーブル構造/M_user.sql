28337cbf831e73d59f88f643a3969ff3a2c1ab73
KENOBR331 <0bakeno1o8@gmail.com>
2025-05-18 08:59:31 +0900

    間違えてSELECT構文をUPしたのでCREATEに修正

--- テーブル構造/M_user.sql
+++ テーブル構造/M_user.sql
@@ -1,15 +1,11 @@
 --*DataTitle 'マスタ_勤怠管理ユーザ'
 --*CaptionFromComment
-SELECT
-    [userid]                                    -- ユーザID
-    , [name]                                    -- ユーザ名
-    , [gender]                                  -- 0
-    , [ipaddress]                               -- ADがないため、IPアドレスで代用
-    , [affiliation]                             -- 所属(部署等を数値で判別する)
-    , [del_flg]                                 -- 1のときは削除とする
-FROM
-    [dbo].[M_user] 
-WHERE
-    [userid] = userid                          -- ユーザID
-ORDER BY
-    [userid]
+create table [dbo].[M_user] (
+  [userid] int not null
+  , [name] nvarchar(30) not null
+  , [gender] bit
+  , [ipaddress] nchar(15) not null
+  , [affiliation] int
+  , [del_flg] bit default 0
+  , primary key (userid)
+);
